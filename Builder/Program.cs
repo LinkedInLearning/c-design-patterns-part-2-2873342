33e6f2cf003a7193bb21cc167766922b579ed4fa
Richard Goforth <rcgoforth@gmail.com>
2021-02-08 15:45:30 -0500

    02_02 UriBuilder Console

---
+++ Builder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var uriBuilder = new UriBuilder("https://linkedin.com/learning");
+            
+            Console.WriteLine(uriBuilder.Host);
+            Console.WriteLine(uriBuilder.Path);
+            Console.WriteLine(uriBuilder.Port);
+
+            var client = new HttpClient();
+
+            var resp = await client.GetAsync(uriBuilder.Uri);
+            Console.WriteLine(resp.StatusCode);
+
+            uriBuilder.Path = "/learning/blazor-getting-started";
+            resp = await client.GetAsync(uriBuilder.Uri);
+            Console.WriteLine(resp.StatusCode);
+
+        }
+    }
+}
