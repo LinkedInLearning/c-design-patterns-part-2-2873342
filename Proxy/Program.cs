43e713613c478a90a6d855fbf5beaa863a136e27
Richard Goforth <rcgoforth@gmail.com>
2021-02-09 22:58:53 -0500

    04_02 Simple Blocking Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Parallel.For(0,10, i => OneFileAtATimeProxy.AppendAllTextAsync("A.txt", i+", "));
+        }
+    }
+
+    static class OneFileAtATimeProxy
+    {
+        static readonly object FileInUse = new object();
+
+        public static Task AppendAllTextAsync(string filename, string text)
+        {
+            lock(FileInUse)
+            { 
+                return File.AppendAllTextAsync(filename, text);                
+            }
+        } 
+    }
+   
+}
