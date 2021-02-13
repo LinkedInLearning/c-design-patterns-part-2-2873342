8328ef5c3e5bc6faa448152e56125c30f351f4d5
Rich Goforth <rcgoforth@gmail.com>
2021-02-13 17:50:47 -0500

    03_05 Challenge Completion

--- Command/Program.cs
+++ Command/Program.cs
@@ -1,45 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Command
 {
-
     class Program
     {
         public static int x = 0;
         public static int y = 0;
         static void Main(string[] args)
         {
-            var commandList = new List<ICommand>();
+             //command, and boolean is true for invoke, and false for undo
+            var commandList = new List<(ICommand, bool)>();
+
             Console.WriteLine("Use Arrows to add direction commands, and enter to run the command list");
 
-            while(true)
+            while (true)
             {
                 var key = Console.ReadKey(true).Key;
                 if (key == ConsoleKey.UpArrow)
-                    commandList.Add(new UpCommand());
+                    commandList.Add((new UpCommand(), true));
                 else if (key == ConsoleKey.DownArrow)
-                    commandList.Add(new DownCommand());
+                    commandList.Add((new DownCommand(), true));
                 else if (key == ConsoleKey.LeftArrow)
-                    commandList.Add(new LeftCommand());
+                    commandList.Add((new LeftCommand(), true));
                 else if (key == ConsoleKey.RightArrow)
-                    commandList.Add(new RightCommand());
+                    commandList.Add((new RightCommand(), true));
+                else if (key == ConsoleKey.Backspace)
+                {
+                    var notUndoneCommands = commandList
+                        .Where(c => c.Item2)
+                        .SkipLast(commandList.Count(c => !c.Item2));
+
+                    if (notUndoneCommands.Any())
+                        commandList.Add((notUndoneCommands.Last().Item1, false));
+                }
                 else if (key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+
+                    commandList.ForEach(c =>
                     {
-                        Console.WriteLine();
-                        commandList.ForEach(c => c.Invoke());
-                        Console.WriteLine($": {x}, {y}");
-                        commandList.Clear();
-                    }
-                
+                        if (c.Item2)
+                            c.Item1.Invoke();
+                        else
+                            c.Item1.Undo();
+                    });
+                    
+                    Console.WriteLine($": {x}, {y}");
+                    
+                    commandList.Clear();
+                }
+
             }
-        
+
         }
     }
 
     interface ICommand
     {
         void Invoke();
+        void Undo();
     }
 
     class UpCommand : ICommand
@@ -49,6 +70,11 @@ namespace Command
             Console.Write("Up ");
             Program.y++;
         }
+        public void Undo()
+        {
+            Console.Write("Down ");
+            Program.y--;
+        }
     }
     class DownCommand : ICommand
     {
@@ -56,6 +82,11 @@ namespace Command
         {
             Console.Write("Down ");
             Program.y--;
+        }
+        public void Undo()
+        {
+            Console.Write("Up ");
+            Program.y++;
         }
     }
     class LeftCommand : ICommand
@@ -65,6 +96,11 @@ namespace Command
             Console.Write("Left ");
             Program.x--;
         }
+        public void Undo()
+        {
+            Console.Write("Right ");
+            Program.x++;
+        }
     }
     class RightCommand : ICommand
     {
@@ -72,6 +108,11 @@ namespace Command
         {
             Console.Write("Right ");
             Program.x++;
+        }
+        public void Undo()
+        {
+            Console.Write("Left ");
+            Program.x--;
         }
     }
 }
