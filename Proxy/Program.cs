6f6f8c10fc3081ed5cb8d74918a3809eef098197
Rich Goforth <rcgoforth@gmail.com>
2021-02-13 18:51:00 -0500

    04_02 File Guard Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 10, i => OneFileAtATimeProxy.AppendAllText("A.txt", i + ", "));
+        }
+    }
+    static class OneFileAtATimeProxy
+    {
+        static readonly object FileInUse = new object();
+        public static void AppendAllText(string filename, string text)
+        {
+            lock (FileInUse)
+            {
+                File.AppendAllText(filename, text);
+            }
+        }
+    }
+}
